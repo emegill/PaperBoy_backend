7835d394a8b15f3ca9008a2638c1d9835b4eeeb8
Matt Lomet <mattlomet@live.com>
2017-06-11 16:22:28 -0400

    backend for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    def index
+    loggedUser = User.where(username: params[:name]).first
+
+    if loggedUser.password === params[:password]
+
+        user_id = loggedUser.id
+
+        loggedInState = {views: true, user_id: user_id }
+
+        render json: loggedInState
+
+    else
+        loggedInState = false
+        render json:  loggedInState
+    end
+
+    end
+
+    def create
+
+        User.create(user_params)
+
+    end
+
+    private
+
+    def user_params
+        params.require(:data).permit(:name, :password)
+    end
 end
