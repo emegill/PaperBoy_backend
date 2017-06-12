770b36f65d5dcd4a33ff40f312f3c57bd2af45d5
Ethan Megill <megill1352@gmail.com>
2017-06-12 10:46:39 -0400

    origial scrapers

--- app/news_scrapers/scraper.rb
+++ app/news_scrapers/scraper.rb
@@ -15,7 +15,7 @@ attr_accessor :parse_page
         story = item_container.css(".Promo-title").children.map { |story| story.text }.compact
     end
 
-    def get_author
+    def get_aurthor
         aurthor = item_container.css(".Promo-author").children.map { |aurthor| aurthor.text }.compact
     end
 
@@ -32,13 +32,14 @@ attr_accessor :parse_page
 
 
         story = get_story
-        author = get_author
+        aurthor = get_aurthor
         content = get_content
         date = get_date
 
 
         (0...story.size).each do |index|
-             News.create(story[index], author: author[index], content: content[index])
+            puts "- - - index: #{index +1} - - - "
+            puts "Story: #{story[index]} | aurthor: #{aurthor[index]} | content: #{content[index]} | date: #{date[index]}"
 
         end
 
@@ -49,9 +50,9 @@ attr_accessor :parse_page
     def item_container
         parse_page.css(".Homepage-section")
     end
-
-
 end
 
-# scraper = Scraper.new
-# scraper.run_scraper
+
+
+scraper = Scraper.new
+scraper.run_scraper
