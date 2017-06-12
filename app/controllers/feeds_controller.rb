6d092058a5ddd9b6ec2b5d3becc1e88c00777a1d
George Xia <georxia@gmail.com>
2017-06-12 17:05:29 -0400

    can return all articles from users in controller feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,7 +2,16 @@ class FeedsController < ApplicationController
   def index
   feed_id = params[:feed_id]
 
-  articles = Feed.find(feed_id).newssites
-  render json: articles.articles
+
+  newssites = Feed.find(feed_id).newssites
+  newssites_json = newssites.as_json
+
+
+  newssites_json. each_with_index do |newssite, index|
+
+    newssite[:articles] =  newssites[index].articles
+  end
+
+  render json: newssites_json
   end
 end
