3b44f76ec7b8cc61809861736c72eb3e881a5f3f
Ian Zolitor <ianzolitor@gmail.com>
2017-06-12 15:30:16 -0400

    guardian api

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,40 @@ def index
     render json: Article.all
 end
 
+def nyt
+
+api_call = Nyt.new
+api_call.get_articles
+end
+
+def create
+  p guardian_params
+guardian_params["articles"].first(5).each do |article|
+
+Article.create(title:article["title"], author:article["author"], content:article["description"], url:article["url"], date:article["publishedAt"], newssite_id:2)
+
+  end
+
+end
+
+
 
 def scrape
     scraper = Scraper.new
     results = scraper.run_scraper
     # render json: results
     5.times do |index|
-    Article.create!(title: results[:story][index], author: results[:author][index], content: results[:content][index], newssite_id: 2)
+    Article.create!(title: results[:story][index], author: results[:author][index], content: results[:content][index], newssite_id:2)
     end
+
+end
+
+private
+
+def guardian_params
+
+params.require(:guardian).permit!
+
 
 end
 
