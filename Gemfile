cbc2d968acb753edcfe8102555f3656594014573
Ian Zolitor <ianzolitor@gmail.com>
2017-06-12 10:05:31 -0400

    monday morning

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 gem "times_wire", "~> 0.9.0"
+gem 'aylien_news_api'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
--- app/controllers/newssites_controller.rb
+++ app/controllers/newssites_controller.rb
@@ -1,10 +1,25 @@
 require "net/http"
 require "uri"
+require 'guardian-content'
+require 'aylien_news_api'
+
 class NewssitesController < ApplicationController
 
 
     def index
-    
+      uri = URI("https://api.nytimes.com/svc/news/v3/content/all/all.json")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  uri.query = URI.encode_www_form({
+    "api-key" => "f18324e1f4af412a9dae2869f9329a95"
+  })
+  request = Net::HTTP::Get.new(uri.request_uri)
+  @result = JSON.parse(http.request(request).body)
+  puts @result.inspect
+  render json: @result
+
+
 
     end
+
 end
