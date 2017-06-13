fce82b044f54536a9ea61f2612e47d07843d91b3
Matt Lomet <mattlomet@live.com>
2017-06-13 11:42:33 -0400

    selector work

--- app/controllers/chosensites_controller.rb
+++ app/controllers/chosensites_controller.rb
@@ -5,11 +5,22 @@ class ChosensitesController < ApplicationController
 
   end
 
+  def destroy
+      site = Chosensite.where!(newssite_id: chosensite_delete_params).first
+
+      site.delete!
+  end
+
   private
 
   def chosensite_params
 
     params.require(:data).permit(:feed_id, :newssite_id)
+  end
+
+  def chosensite_delete_params
+      params.require(:chosensite).permit(:newssite_id)
+
   end
 
 end
