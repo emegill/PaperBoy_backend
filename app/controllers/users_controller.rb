78231ccdc63066302d23898ecf5fbb3040bfdb38
Ian Zolitor <ianzolitor@gmail.com>
2017-06-13 12:12:06 -0400

    users controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,29 +3,36 @@ class UsersController < ApplicationController
     logging_in_user = User.where(username: params[:username]).first
     p logging_in_user
 
-    if logging_in_user.password === params[:password]
+
+   if logging_in_user.password === params[:password]
       logging_users = logging_in_user.id
 
-      logged_in_state = {views: true, user_id: logging_users}
+     feed_id = logging_in_user.feed.id
+      logged_in_state = {views: true, user_id: logging_users,feed_id: feed_id}
       render json: logged_in_state
 
-    else
+   else
         loggedInState = false
         render json:  loggedInState
     end
   end
 
-  def create
+ def create
     User.create(user_params)
     user_name = user_params[:username]
-    user_id = User.where(username: user_name).first.id
+    user = User.where(username: user_name).first
+    user_id = user.id
     Feed.create(user_id: user_id)
+    feed_id = user.feed.id
 
-    end
+   logged_in_state = {views: true, user_id: user_id,feed_id: feed_id}
+    render json: logged_in_state
 
-    private
+   end
 
-    def user_params
+   private
+
+   def user_params
         params.require(:data).permit(:username, :password)
     end
 end
